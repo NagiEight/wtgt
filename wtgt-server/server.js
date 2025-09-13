210f449364e17f06f88328fdd04d605709e3bce6
Zizel <danbua999@gmail.com>
2025-09-13 17:36:51 +0700

    pls test 2

--- wtgt-server/server.js
+++ wtgt-server/server.js
@@ -2,107 +2,113 @@ import http from "http";
 import ws from "ws";
 import crypto from "crypto";
 
+const PORT = 3000;
+
 const users = {
-    /*  "UserID": {
-     *      "UserName": Claire Iidea,
-     *      "avt": <bytes>
-     *  }
-     * */
+	/*	"UserID": {
+	 *		"UserName": Claire Iidea,
+	 *		"avt": <bytestring>
+	 *	}
+	 * */
 };
 
 const messages = {
-    /*  "MessageID": {
-     *      "SenderID": SenderID,
-     *      "content": "Hello World!",
-     *      "timestamp": somethingsomething
-     *  }
-     * */
+	/*	"MessageID": {
+	 *		"SenderID": SenderID,
+	 *		"content": "Hello World!",
+	 *		"timestamp": somethingsomething
+	 *	}
+	 * */
 };
 
 const server = http.createServer((req, res) => {
-    
+
 });
 
 const wss = new ws.Server({server});
 
 wss.on("connection", (client, req) => {
-    const IP = req.socket.remoteAddress;
-    const UserID = crypto.createHash("sha256").update(IP).digest("hex");
+	const IP = req.socket.remoteAddress;
+	const UserID = crypto.createHash("sha256").update(IP).digest("hex");
 
-    console.log(`New connection fron ${IP}.`);
-    client.send(JSON.stringify({ type: "init", messages, users }));
+	console.log(`New connection fron ${IP}.`);
+	client.send(JSON.stringify({ type: "init", messages, users }));
 
-    client.on("message", (content, isBinary) => {
-        if(isBinary) {
-            users[UserID].avt = content.toString("base64");
-            wss.clients.forEach((c) => {
-                if(c.readyState === ws.OPEN) {
-                    const toSendJSON = {
-                        type: "avt",
-                        content: {
-                            UserID,
-                            avt: content.toString("base64")
-                        }
-                    }
+	client.on("message", (content, isBinary) => {
+		if(isBinary) {
+			users[UserID].avt = content.toString("base64");
+			wss.clients.forEach((c) => {
+				if(c.readyState === ws.OPEN) {
+					const toSendJSON = {
+						type: "avt",
+						content: {
+							UserID,
+							avt: content.toString("base64")
+						}
+					}
 
-                    c.send(JSON.stringify(toSendJSON));
-                }
-            });
-        }
-        else {
-            const ContentJSON = JSON.parse(content.toString());
+					c.send(JSON.stringify(toSendJSON));
+				}
+			});
+		}
+		else {
+			const ContentJSON = JSON.parse(content.toString());
 
-            if(ContentJSON.type === "message") {
-                const msgID = crypto.createHash("sha256")
-                        .update(UserID.concat(ContentJSON.content, Object.keys(messages).length.toString()))
-                        .digest("hex");
+			if(ContentJSON.type === "message") {
+				const msgID = crypto.createHash("sha256")
+						.update(UserID.concat(ContentJSON.content, Object.keys(messages).length.toString()))
+						.digest("hex");
 
-                const msgObj = {
-                    SenderID: UserID,
-                    content: ContentJSON.content,
-                    timestamp: Date.now()
-                } 
+				const msgObj = {
+					SenderID: UserID,
+					content: ContentJSON.content,
+					timestamp: Date.now()
+				} 
 
-                messages[msgID] = msgObj;
+				messages[msgID] = msgObj;
 
-                wss.clients.forEach((c) => {
-                    if(c.readyState === ws.OPEN) {
-                        const toSendJSON = {
-                            type: "message",
-                            content: {
-                                info: msgObj
-                            }
-                        }
+				wss.clients.forEach((c) => {
+					if(c.readyState === ws.OPEN) {
+						const toSendJSON = {
+							type: "message",
+							content: {
+								info: msgObj
+							}
+						}
 
-                        c.send(JSON.stringify(toSendJSON));
-                    }
-                });
-            }
-            else if(ContentJSON.type === "member") {
-                users[UserID].UserName = ContentJSON.UserName;
-                const ContentJSON = {
-                    type: "member",
-                    content: {
-                        UserID,
-                        UserName: ContentJSON.UserName
-                    }
-                }
-            }
-            else {
-                client.send(`Unknown content type: ${ContentJSON.type}.`)
-            }
-        }
-    });
+						c.send(JSON.stringify(toSendJSON));
+					}
+				});
+			}
+			else if(ContentJSON.type === "member") {
+				users[UserID].UserName = ContentJSON.UserName;
+				const ContentJSON = {
+					type: "member",
+					content: {
+						UserID,
+						UserName: ContentJSON.UserName
+					}
+				}
+			}
+			else {
+				client.send(`Unknown content type: ${ContentJSON.type}.`)
+			}
+		}
+	});
 });
 
-/*  {
- *      "type": "message"
- *      "content": "hello world!"
- *  }
+/*	{
+ *		"type": "message"
+ *		"content": "hello world!"
+ *	}
  * */
 
-/*  {
- *      "type": "member",
- *      "UserName": "Claire Iidea"
- *  }
+/*	{
+ *		"type": "member",
+ *		"UserName": "Claire Iidea"
+ *	}
  * */
+
+server.listen(PORT, () => {
+	console.log(`Hello World! Server's running at port ${PORT}.`)
+});
